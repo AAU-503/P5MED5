71a3ab1585e7a8d28ec7e6df4d809d5d020122e9
0x16e8bea <0x16e8bea@gmail.com>
2016-10-29 22:33:00 +0200

    Added variables to ScoreManager

--- Assets/Scripts/Camera/ScoreManager.cs
+++ Assets/Scripts/Camera/ScoreManager.cs
@@ -5,6 +5,14 @@ public class ScoreManager : MonoBehaviour {
 
     static float playerScore = 0;
 
+	static public int coinsScore = 10;
+	static public int boxHitScore = 2;
+	static public int boxFailScore = -5;
+	static public int enemyKillScore = 10;
+	static public int enemyFailScore = -10;
+	static public int lavaScore = -15;
+
+
 	// Update is called once per frame
 	void Update () {
         playerScore += Time.deltaTime * 10;
@@ -17,7 +25,8 @@ public class ScoreManager : MonoBehaviour {
     }
 
     void OnGUI()
-    {
-        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore));
-    }
+	{
+		GUI.Label (new Rect (10, 10, 100, 30), "Score: " + (int)(playerScore));
+	}
+    
 }
