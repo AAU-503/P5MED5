8797254daf12e086a05935ad3349fdb21e6ab47a
0x16e8bea <0x16e8bea@gmail.com>
2016-10-29 20:57:10 +0200

    Added camera shake (semi-functional)

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     public Transform player;
+
     public float speed = 0.1f; 
+	public Vector3 startPos;
+	public float xPos;
+	public float shakeDuration;
 
 	void Start () {
-	
+		startPos = transform.position;
 	}
 	
 	void Update () {
-        transform.Translate(speed, 0, 0, Space.World);
+		xPos += speed;
+
+        //transform.Translate(speed, 0, 0, Space.World);
+
+		Shake (0f);
+
+	}
+
+	void Shake(float shakeAmount) {
+
+		transform.position = startPos + ((Random.insideUnitSphere) * shakeAmount) + new Vector3(xPos, 0, 0);
+
 	}
 }
