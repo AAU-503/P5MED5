ed00c3c1091ef66475a731183072ac5a0fde24dc
0x16e8bea <0x16e8bea@gmail.com>
2016-10-29 22:41:40 +0200

    Removed a curly-bracket in PlayerBehavior.cs and deleted Jump.cs

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -2,53 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehavior : MonoBehaviour {
+public class PlayerBehavior : MonoBehaviour
+{
 
-    public CameraController cameraController;
-    public ScoreManager scoreManager;
+	public CameraController cameraController;
+	public ScoreManager scoreManager;
 	private CharacterController controller;
 
 	private Vector3 moveDirection = Vector3.zero;
 	public float jumpSpeed = 20.0f;
 	public float gravity = 2.0f;
 
-    private bool attacked = true;
+	private bool attacked = true;
 
-    // Use this for initialization
-    void Start() {
-        cameraController = GameObject.Find("Main Camera").GetComponent<CameraController>();
-		controller = GetComponent<CharacterController>();
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		cameraController = GameObject.Find ("Main Camera").GetComponent<CameraController> ();
+		controller = GetComponent<CharacterController> ();
+	}
 
-    // Update is called once per frame
-    void Update() {
+	// Update is called once per frame
+	void Update ()
+	{
 		
 		Jump ();
 		Movement ();
-    }
-
-    void OnTriggerEnter(Collider other) { // Ability to pick up coins adding to score in ScoreManager and attacking boxes
-        
-        print(other.gameObject);
-        
-        if (other.gameObject.CompareTag("Coin")) {
-			Destroy(other.gameObject);
-			ScoreManager.ChangeScore(ScoreManager.coinsScore);
-        }
-
-        if (other.gameObject.tag == "Enemy") {
-			ScoreManager.ChangeScore(ScoreManager.enemyFailScore);
-        }
-    }
-
-	void Movement() {
-		transform.Translate(cameraController.speed, 0, 0);
 	}
 
-	void Jump() {
+	void OnTriggerEnter (Collider other)
+	{ // Ability to pick up coins adding to score in ScoreManager and attacking boxes
+        
+		print (other.gameObject);
+        
+		if (other.gameObject.CompareTag ("Coin")) {
+			Destroy (other.gameObject);
+			ScoreManager.ChangeScore (ScoreManager.coinsScore);
+		}
+
+		if (other.gameObject.tag == "Enemy") {
+			ScoreManager.ChangeScore (ScoreManager.enemyFailScore);
+		}
+	}
+
+	void Movement ()
+	{
+		transform.Translate (cameraController.speed, 0, 0);
+	}
+
+	void Jump ()
+	{
 		if (controller.isGrounded) {
 
-			if (Input.GetKeyDown(KeyCode.Space)) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
 				moveDirection.y = jumpSpeed;
 			}
 
@@ -56,7 +62,7 @@ public class PlayerBehavior : MonoBehaviour {
 			moveDirection.y -= gravity;
 		}
 
-		controller.Move(moveDirection * Time.fixedDeltaTime);
-		}
+		controller.Move (moveDirection * Time.fixedDeltaTime);
 	}
+
 }
