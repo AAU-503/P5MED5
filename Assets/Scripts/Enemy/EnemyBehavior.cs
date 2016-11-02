c16751b8a3a1261027eecc3db8ca2a133656f43b
Thunderless <mathundert@gmail.com>
2016-11-02 14:10:51 +0100

    Enemy Movement
    Added enemyStates
    - Booleans for seperate or combined enemy movement on x and y axis

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour {
     //speed can be adjusted to make them harder or easier
     public float speed = 1;
+    public float flyHeight = 2;
 
+    public bool isMovingX;
+    public bool isMovingY;
+    private bool isMovingUp;
     public bool isDestroyed = false;
 
     void Start() {
-
     }
 
     // Update is called once per frame
-    void Update () {
-		 transform.Translate(new Vector3(-speed * Time.deltaTime, Mathf.Cos(Time.time * 10) / 100, 0));
-
+    void Update() {
+        if (isMovingX) {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, Mathf.Cos(Time.time * 10) / 100, 0));
+        }
+        if (isMovingY)
+        {
+            if (isMovingUp)
+            {
+                transform.Translate(new Vector3(0, +speed * Time.deltaTime, 0));
+            }
+            if (!isMovingUp)
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+            if (!isMovingX && !isMovingY)
+            {
+                transform.Translate(new Vector3(0, Mathf.Cos(Time.time * 10) / 100, 0));
+            }
+        }
         if (isDestroyed)
         {
             Destroy(gameObject);
+        }
+        if (isMovingUp && transform.position.y >= flyHeight)
+        {
+            isMovingUp = false;
+        }else if (!isMovingUp && transform.position.y <= 0)
+        {
+            isMovingUp = true;
         }
     }
 
