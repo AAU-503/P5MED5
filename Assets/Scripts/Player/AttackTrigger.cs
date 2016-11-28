3bc0d636de8d3671bcbe58f7e9102177b14b6980
Maltejb <mbredd14@student.aau.dk>
2016-11-28 12:15:37 +0100

    Revert "SCORE TOAST"
    This reverts commit dad3a2d695abf309504eed6ad7631cf2b2d69ba6.

--- Assets/Scripts/Player/AttackTrigger.cs
+++ Assets/Scripts/Player/AttackTrigger.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class AttackTrigger : MonoBehaviour {
 
-    public GameObject toast;
-
-
     private bool attacked = true;
 
     void OnTriggerEnter(Collider col)
@@ -14,18 +11,15 @@ public class AttackTrigger : MonoBehaviour {
 
         if(col.CompareTag("Box")) {
             col.gameObject.GetComponent<BoxBehavior> ().Attacked();
-            Instantiate(toast, col.transform.position + new Vector3(3.0f,2.0f,-2.0f), Quaternion.identity).GetComponentInChildren<ScoreText>().setText(ScoreManager.boxHitScore);
         }
 
         if(col.CompareTag("Explosive")) {
             col.gameObject.GetComponent<ExplosiveBehavior>().Attacked();
-            Instantiate(toast, col.transform.position + new Vector3(3.0f,2.0f,-2.0f), Quaternion.identity).GetComponentInChildren<ScoreText>().setText(ScoreManager.explosiveHitScore);
         }
 
         if (col.gameObject.tag == "Enemy") {
             ScoreManager.ChangeScore(ScoreManager.enemyKillScore);
 			col.gameObject.GetComponent<EnemyBehavior> ().Attacked();
-            Instantiate(toast, col.transform.position + new Vector3(3.0f,2.0f,-2.0f), Quaternion.identity).GetComponentInChildren<ScoreText>().setText(ScoreManager.enemyKillScore);
 
         }
     }	
